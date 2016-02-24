a8e5e982470fd575972368ef5cbc61e3cf158e66
David A <dra151994@hotmail.com>
2016-02-23 23:46:14 -0800

    Fixing file references.

--- AES.SecuritySvc/ISecuritySvc.cs
+++ AES.SecuritySvc/ISecuritySvc.cs
@@ -11,14 +11,14 @@ namespace AES.SecuritySvc
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface ISecurity
+    public interface ISecuritySvc
     {
 
         [OperationContract]
         ApplicantInfoContract ValidateUser(ApplicantInfoContract userInfo);
 
         [OperationContract]
-        bool Logout(ApplicantInfoContract userInfo);
+        ApplicantInfoContract GetUser(ApplicantInfoContract user);
     }
     
 }
