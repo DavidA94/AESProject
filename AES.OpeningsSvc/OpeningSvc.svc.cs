6ce0e72a755163bb1d1807597fd62a374e87a14b
Random Davis <random.davis@gmail.com>
2016-02-26 17:42:08 -0800

    Openings -> OpeningSvc

--- AES.OpeningsSvc/OpeningSvc.svc.cs
+++ AES.OpeningsSvc/OpeningSvc.svc.cs
@@ -12,7 +12,7 @@ namespace AES.OpeningsSvc
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Openings : IOpeningSvc
+    public class OpeningSvc : IOpeningSvc
     {
         public List<JobOpeningContract> GetOpenings(int StoreID)
         {
