60268842693b0480f19cae1ae80fc2c3cffe1b06
Random Davis <random.davis@gmail.com>
2016-04-12 18:38:28 -0700

    Adds employee user contract

---
+++ AES.Shared/Contracts/EmployeeUserContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AES.Shared.Contracts
+{
+    [DataContract]
+    public class EmployeeUserContract
+    {
+        [DataMember]
+        public int Email { get; set; }
+
+        [DataMember]
+        public EmployeeRole Role { get; set; }
+
+        [DataMember]
+        public UserInfoContract UserInfo { get; set; }
+    }
+}
