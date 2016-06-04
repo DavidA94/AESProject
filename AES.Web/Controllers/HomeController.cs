dc1576d11d95470979d2f8766cf65c466afb66bd
hpainter2002 <hpainter2002@gmail.com>
2016-06-03 22:25:05 -0700

    Switch to Laptop Changes: Added HomePage, Refactored IT, SE,  and SM Dashboards.

--- AES.Web/Controllers/HomeController.cs
+++ AES.Web/Controllers/HomeController.cs
@@ -8,29 +8,23 @@ namespace AES.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult HomePage()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult HomePage(string BeginButton)
+        {
+            if (BeginButton == "True")
+            {
+                return RedirectToAction("Login", "ApplicantLogin");
+            }
+
+            return View();
+        }
+
+
 
     }
 }
