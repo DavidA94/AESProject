39a52cc8fc10e0a03ca09d74dad8ca1c7d6da344
Random Davis <random.davis@gmail.com>
2016-06-04 14:45:38 -0700

    Fixes bug where the home page gave a 404 error

--- AES.Web/App_Start/RouteConfig.cs
+++ AES.Web/App_Start/RouteConfig.cs
@@ -48,7 +48,7 @@ namespace AES.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "HomePage", id = UrlParameter.Optional }
             );
         }
     }
